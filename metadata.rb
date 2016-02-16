f8c065e5d3f777aadfaaa7e91ee1e68c4c757929
Levon Becker <levon.becker@bonusbits.com>
2016-02-15 16:11:37 -0800

    Initial Commit

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,8 @@
-# rubocop:disable Style/SingleSpaceBeforeFirstArg
-
 # CookBook Information
-name             'bonusbits_library'
-maintainer       'Levon Becker'
+name 'bonusbits_library'
+maintainer 'Levon Becker'
 maintainer_email 'levon.becker.github@bonusbits.com'
-license          'All rights reserved'
-description      'Shared LWRP and Libraries'
+license 'All rights reserved'
+description 'Shared LWRP and Libraries'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version '1.0.0'
--- resources/output.rb
+++ resources/output.rb
@@ -3,5 +3,5 @@
 actions :display_output
 default_action :display_output
 
-attribute :name, :kind_of => String, :name_attribute => true, :default => 'RESULTS'
+attribute :name, :kind_of => String, :name_attribute => true
 attribute :reports, :kind_of => Array
